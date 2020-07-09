2619358781cc7f66e32bc90aa3c4dfc01a057c64
Purushottam Sharma <p.sharma@salubrioustech.com>
2020-07-09 13:25:11 +0530

    Modified user serializer

--- app/serializers/centre_serializer.rb
+++ app/serializers/centre_serializer.rb
@@ -1,3 +1,3 @@
 class CentreSerializer < ActiveModel::Serializer
-  attributes :id, :store_code, :centre_title, :centre_address, :centre_type, :fixed_payment, :revenue_share_consult, :status
+  attributes :id, :store_code, :centre_title, :centre_address, :centre_type, :fixed_payment, :revenue_share, :status
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -10,10 +10,95 @@ class UserSerializer < ActiveModel::Serializer
     end
   end
 
+  def city
+    if !object[:city_id].blank?
+      { id: self.object.city.id, name: self.object.city.city_name }
+    end
+  end
+
+  def state
+    if !object[:state_id].blank?
+      { id: self.object.state.id, name: self.object.state.state_name }
+    end
+  end
+
+  def country
+    if !object[:country_id].blank?
+      { id: self.object.country.id, name: self.object.country.country_name }
+    end
+  end
+
+  def default_language
+    if !object[:default_language].blank?
+  	 {key: object[:default_language], value: object[:default_language].capitalize()}
+    end
+  end
+
+  def doctor_category
+  	if !object[:doctor_category_id].blank?
+    	{ id: self.object.doctor_category.id, category_title: self.object.doctor_category.category_title }
+    end
+  end
+
+  # def role
+  #   if !object[:role].blank?
+  #   	role = Role.find_by_role(object[:role])
+  #   	role.role_title if role
+  #   end
+  # end
+
   def doctor_type
   	if !object[:is_internal_doctor].blank? && object[:role] == 'doctor'
 	  	hash = {"0" => "External", "1" => "Internal"}
 	  	hash["#{object[:is_internal_doctor]}"]
 	  end
+  end
+
+  def is_email_confirmed
+  	if !object[:is_email_confirmed].blank?
+	  	hash = {"0" => "Pending", "1" => "Confirmed"}
+	  	{ key: object[:is_email_confirmed], value: hash["#{object[:is_email_confirmed]}"]}
+	  end
+  end
+
+  def is_verified
+  	if !object[:is_verified].blank?
+	  	hash = {"0" => "Pending", "1" => "Verified"}
+	  	{ key: object[:is_verified], value: hash["#{object[:is_verified]}"]}
+	  end
+  end
+
+  def is_approved
+  	if !object[:is_approved].blank?
+	  	hash = {"0" => "Pending", "1" => "Approved"}
+	  	{ key: object[:is_approved], value: hash["#{object[:is_approved]}"]}
+	  end
+  end
+
+  def is_editable
+  	if !object[:is_editable].blank?
+	  	hash = {"0" => "Non Editable", "1" => "Editable"}
+	  	{ key: object[:is_editable], value: hash["#{object[:is_editable]}"]}
+	  end
+  end
+
+  def status
+  	if !object[:status].blank?
+	  	hash = {"0" => "Inactive", "1" => "Active"}
+	  	{ key: object[:status], value: hash["#{object[:status]}"]}
+	  end
+  end
+
+  def is_qualified_mso
+  	if !object[:is_qualified_mso].blank?
+	  	hash = {"0" => "Trainee", "1" => "Qualified"}
+	  	{ key: object[:is_qualified_mso], value: hash["#{object[:is_qualified_mso]}"]}
+	  end
+  end
+
+  def centre
+    if !object[:centre_id].blank?
+      { id: self.object.centre.id, store_code: self.object.centre.store_code, centre_title: self.object.centre.centre_title, centre_address: self.object.centre.centre_address, centre_type: self.object.centre.centre_type, fixed_payment: self.object.centre.fixed_payment, revenue_share: self.object.centre.revenue_share, status: self.object.centre.status }
+    end
   end
 end
